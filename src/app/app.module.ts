dacaf986985a4ff12d857d389b48f09e11caf1fd
bayuSarifudin <bayusarifudin@gmail.com>
2022-10-18 19:07:36 +0700

    make a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { HeloComponent } from './helo/helo.component';
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent],
+  declarations: [AppComponent, BooksComponent, HeloComponent],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
